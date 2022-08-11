04f0519c204c05b2557c2d366061517c7c13d98b
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 12:38:33 -0700

    Add fabUnitedStatesStreetTypeAsString.swift

---
+++ Shared/Fab/fabUnitedStatesStreetTypeAsString.swift
@@ -0,0 +1,19 @@
+public func fabUnitedStatesStreetTypeName() -> String {
+    [
+        "Avenue",
+        "Bay",
+        "Boulevard",
+        "Circle",
+        "Court",
+        "Cove",
+        "Drive",
+        "Expressway",
+        "Lane",
+        "Parkway",
+        "Place",
+        "Road",
+        "Row",
+        "Street",
+        "Way",
+    ].randomElement()!
+}
