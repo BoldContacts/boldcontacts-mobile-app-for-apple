1404f959ec7cbf059371bab58c7db15b77556376
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 15:55:44 -0700

    Add main instantiation

--- Shared/BoldContactsApp.swift
+++ Shared/BoldContactsApp.swift
@@ -1,20 +1,43 @@
-//
-//  BoldContactsApp.swift
-//  Shared
-//
-//  Created by jph on 8/11/22.
-//
-
 import SwiftUI
+import Contacts
+import os
 
 @main
 struct BoldContactsApp: App {
     let persistenceController = PersistenceController.shared
 
+    // Connect to app delegate class defined in AppDelegate.swift
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var cursor: Cursor
+
+    init() {
+        logger.info("BoldContacts init")
+        CNContactStore().requestAccess(for: .contacts) { (access, error) in
+            logger.info("Access: \(access)")
+        }
+        if true /* if this is a development run */ {
+            demoDeleteContactForMe()
+            demoCreateContactForMe()
+            demoDeleteContactsForABCDEF()
+            demoCreateContactsForABCDEF()
+        }
+        cursor = Cursor(list: CNContactStore.list())
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            ContentView(cursor: cursor)
         }
     }
+    
+}
+    
+//Original:
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+//        }
+//    }
 }
