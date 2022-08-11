0ca5ea66e6813fe30d8e30f0e49de0ce351ff672
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 12:38:05 -0700

    Add fabGemName.swift

---
+++ Shared/Fab/fabGemName.swift
@@ -0,0 +1,32 @@
+public func fabGemName() -> String {
+    [
+        "Amber",
+        "Beryl",
+        "Coral",
+        "Diamond",
+        "Emerald",
+        "Feldspar",
+        "Garnet",
+        "Heliodor",
+        "Ivory",
+        "Jasper",
+        "Kunzite",
+        "Lolite",
+        "Malacite",
+        "Nephrite",
+        "Opal",
+        "Pearl",
+        "Quartz",
+        "Ruby",
+        "Sapphire",
+        "Topaz",
+        "Uvite",
+        "Verdite",
+        "Wavellite",
+        "Xanthite",
+        "Yuksporite",
+        "Zircon",
+    ].randomElement()!
+}
+
+
