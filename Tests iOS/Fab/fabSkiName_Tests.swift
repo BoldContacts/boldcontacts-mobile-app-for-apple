b31acde385c9ee3add87b76c706b2eb81fd140ab
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:21:31 -0700

    Add fabSkiName_Tests.swift

---
+++ Tests iOS/Fab/fabSkiName_Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+import PhotoCaller
+
+class fabSkiName_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: String = fabSkiName()
+//        XCTAssert(!x.isEmpty)
+    }
+
+}
