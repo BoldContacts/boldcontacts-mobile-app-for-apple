a16831eaaa29a3aa6f2e8a62faab54d9b23adcc6
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:23:25 -0700

    Add fabPhoneNumberAreaCodeAsString_Tests.swift

---
+++ Tests iOS/Fab/fabPhoneNumberAreaCodeAsString_Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+import PhotoCaller
+
+class fabPhoneNumberAreaCodeAsString_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: String = fabPhoneNumberAreaCodeAsString()
+//        XCTAssert(!x.isEmpty)
+    }
+
+}
