081421c2a6100a1df9530e585b8deb218ea72bce
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:21:16 -0700

    Add fabGemName_Tests.swift

---
+++ Tests iOS/Fab/fabGemName_Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+import PhotoCaller
+
+class fabGemName_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: String = fabGemName()
+//        XCTAssert(!x.isEmpty)
+    }
+
+}
