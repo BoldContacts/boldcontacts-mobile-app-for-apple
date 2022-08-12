65db67e16e18e01eba3989a48bbce3d1417baedb
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:24:35 -0700

    Add fabPostalAddressAsCNPostalAddress_Tests.swift

---
+++ Tests iOS/Fab/fabPostalAddressAsCNPostalAddress_Tests.swift
@@ -0,0 +1,14 @@
+import XCTest
+import PhotoCaller
+
+class fabPostalAddressAsCNPostalAddress_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: CNPostalAddress = fabPostalAddressAsCNPostalAddress()
+//        XCTAssert(!x.street.isEmpty)
+//        XCTAssert(!x.city.isEmpty)
+//        XCTAssert(!x.state.isEmpty)
+//        XCTAssert(!x.postalCode.isEmpty)
+        }
+
+}
