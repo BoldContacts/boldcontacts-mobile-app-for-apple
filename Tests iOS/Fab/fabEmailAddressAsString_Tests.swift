cd831d2c36eb151a7821215f447d48556ca7ba5a
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:23:02 -0700

    Add fabEmailAddressAsString_Tests.swift

---
+++ Tests iOS/Fab/fabEmailAddressAsString_Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+import PhotoCaller
+
+class fabEmailAddressAsString_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: String = fabEmailAddressAsString()
+//        XCTAssert(!x.isEmpty)
+    }
+
+}
