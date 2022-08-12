cd68ed53d2829240a119e9727a85075ab559a6bc
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:21:23 -0700

    Add fabTreeName_Tests.swift

---
+++ Tests iOS/Fab/fabTreeName_Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+import PhotoCaller
+
+class fabTreeName_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: String = fabTreeName()
+//        XCTAssert(!x.isEmpty)
+    }
+
+}
