6f4d4ca3c31d52974af8fd51b942aef99d30afe6
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 19:49:50 -0700

    Add parseStringToURLSafeString_Tests.swift

---
+++ Tests iOS/Parse/parseStringToURLSafeString_Tests.swift
@@ -0,0 +1,12 @@
+import XCTest
+
+class parseStringToURLSafeString_Tests: XCTestCase {
+
+//    func test_parseStringToURLSafeString_OK() throws {
+//        let string = "1234567890-ABCDEF"
+//        let expect = "1234567890-ABCDEF"
+//        let actual = parseStringToURLSafeString(string: string)
+//        XCTAssertEqual(expect, actual)
+//    }
+
+}
