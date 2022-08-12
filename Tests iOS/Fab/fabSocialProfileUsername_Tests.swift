d25dcd7b018fc085a985c5bc66473e08e0230c56
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:23:48 -0700

    Add fabSocialProfileUsername_Tests.swift

---
+++ Tests iOS/Fab/fabSocialProfileUsername_Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+import PhotoCaller
+
+class fabSocialProfileUsername_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: String = fabSocialProfileUsername()
+//        XCTAssert(!x.isEmpty)
+    }
+
+}
