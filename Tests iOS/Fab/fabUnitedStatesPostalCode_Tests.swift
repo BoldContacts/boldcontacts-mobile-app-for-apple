949b5cd0cbe6cb17f2c7edf38fdf275d732669fb
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:22:54 -0700

    Add fabUnitedStatesPostalCode_Tests.swift

---
+++ Tests iOS/Fab/fabUnitedStatesPostalCode_Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+import PhotoCaller
+
+class fabUnitedStatesPostalCode_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: String = fabUnitedStatesPostalCode()
+//        XCTAssert(!x.isEmpty)
+    }
+
+}
