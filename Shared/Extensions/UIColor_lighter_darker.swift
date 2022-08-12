31e4993fab7aea6b7b63b0625a953820c6ad03e2
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:08:50 -0700

    Comment out UIColor_lighter_darker.swift

--- Shared/Extensions/UIColor_lighter_darker.swift
+++ Shared/Extensions/UIColor_lighter_darker.swift
@@ -2,12 +2,12 @@ import UIKit
 
 extension UIColor {
     
-    func lighter(componentDelta: CGFloat = 0.1) -> UIColor {
-        return makeColor(componentDelta: componentDelta)
-    }
+//    func lighter(componentDelta: CGFloat = 0.1) -> UIColor {
+//        return makeColor(componentDelta: componentDelta)
+//    }
     
-    func darker(componentDelta: CGFloat = 0.1) -> UIColor {
-        return makeColor(componentDelta: -1*componentDelta)
-    }
+//    func darker(componentDelta: CGFloat = 0.1) -> UIColor {
+//        return makeColor(componentDelta: -1*componentDelta)
+//    }
 
 }
