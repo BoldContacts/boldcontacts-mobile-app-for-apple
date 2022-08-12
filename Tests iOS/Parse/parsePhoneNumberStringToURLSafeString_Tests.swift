5f27a31a1413ec922ac51daab16abe2fd2f03038
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 19:49:42 -0700

    Add parsePhoneNumberStringToURLSafeString_Tests.swift

---
+++ Tests iOS/Parse/parsePhoneNumberStringToURLSafeString_Tests.swift
@@ -0,0 +1,12 @@
+import XCTest
+
+class parsePhoneNumberStringToURLSafeString_Tests: XCTestCase {
+
+//    func test_parsePhoneNumberStringToURLSafeString_OK() throws {
+//        let string = "123456789"
+//        let expect = "123456789"
+//        let actual = parsePhoneNumberStringToURLSafeString(string: string)
+//        XCTAssertEqual(expect, actual)
+//    }
+
+}
