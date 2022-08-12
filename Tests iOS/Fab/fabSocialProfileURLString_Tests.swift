6037096336baf2018207164adf9c3b1dd63a50be
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:23:39 -0700

    Add fabSocialProfileURLString_Tests.swift

---
+++ Tests iOS/Fab/fabSocialProfileURLString_Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+import PhotoCaller
+
+class fabSocialProfileURLString_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: String = fabSocialProfileURLString()
+//        XCTAssert(!x.isEmpty)
+    }
+
+}
