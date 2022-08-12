b2f2b0ce56c1a063c54f92c60da1c206f4d8e0e4
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:22:23 -0700

    Add fabUnitedStatesStreetTypeName_Tests.swift

---
+++ Tests iOS/Fab/fabUnitedStatesStreetTypeName_Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+import PhotoCaller
+
+class fabStreetTypeName_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: String = fabStreetTypeName()
+//        XCTAssert(!x.isEmpty)
+    }
+
+}
