8711314ec69295826d95c64f81c19b2688853676
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:22:36 -0700

    Add fabUnitedStatesStateCodeAsString_Tests.swift

---
+++ Tests iOS/Fab/fabUnitedStatesStateCodeAsString_Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+import PhotoCaller
+
+class fabUnitedStatesStateCodeAsString_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: String = fabUnitedStatesStateCodeAsString()
+//        XCTAssert(!x.isEmpty)
+    }
+
+}
