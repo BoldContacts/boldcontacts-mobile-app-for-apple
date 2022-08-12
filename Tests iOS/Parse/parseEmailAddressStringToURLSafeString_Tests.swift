435c5523302af3798ea24679e356ba412d904b52
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 19:49:33 -0700

    Add parseEmailAddressStringToURLSafeString_Tests.swift

---
+++ Tests iOS/Parse/parseEmailAddressStringToURLSafeString_Tests.swift
@@ -0,0 +1,12 @@
+import XCTest
+
+class parseEmailAddressStringToURLSafeString_Tests: XCTestCase {
+
+//    func test_parseEmailAddressStringToURLSafeString_OK() throws {
+//        let string = "alpha@example.com"
+//        let expect = "alpha@example.com"
+//        let actual = parseEmailAddressStringToURLSafeString(string: string)
+//        XCTAssertEqual(expect, actual)
+//    }
+
+}
