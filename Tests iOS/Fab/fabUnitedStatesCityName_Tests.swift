30f8f255ec01dab345f7802ef0929a8c97015111
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:22:45 -0700

    Add fabUnitedStatesCityName_Tests.swift

---
+++ Tests iOS/Fab/fabUnitedStatesCityName_Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+import PhotoCaller
+
+class fabUnitedStatesCityName_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: String = fabUnitedStatesCityName()
+//        XCTAssert(!x.isEmpty)
+    }
+
+}
