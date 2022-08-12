d9c1c534e5cb80db780b9486699f17c70b33e949
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:24:23 -0700

    Add fabSocialProfileAsCNSocialProfile_Tests.swift

---
+++ Tests iOS/Fab/fabSocialProfileAsCNSocialProfile_Tests.swift
@@ -0,0 +1,14 @@
+import XCTest
+import PhotoCaller
+
+class fabSocialProfileAsCNSocialProfile_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: CNSocialProfile = fabSocialProfileAsCNSocialProfile()
+//        XCTAssert(!x.urlString.isEmpty)
+//        XCTAssert(!x.username.isEmpty)
+//        XCTAssert(!x.userIdentifier.isEmpty)
+//        XCTAssert(!x.service.isEmpty)
+    }
+
+}
