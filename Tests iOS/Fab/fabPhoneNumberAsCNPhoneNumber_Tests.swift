35587ef6221f0f6ac71954bfb7c5579608922d5c
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:24:29 -0700

    Add fabPhoneNumberAsCNPhoneNumber_Tests.swift

---
+++ Tests iOS/Fab/fabPhoneNumberAsCNPhoneNumber_Tests.swift
@@ -0,0 +1,12 @@
+import XCTest
+import PhotoCaller
+
+class fabPhoneNumberAsCNPhoneNumber_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: CNPhoneNumber = fabPhoneNumberAsCNPhoneNumber()
+//        XCTAssert(!x.stringValue.isEmpty)
+    }
+
+}
+
