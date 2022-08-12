01ea7a41bc3294956314df1ec0ea66700b837829
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:23:11 -0700

    Add fabPhoneNumberAsString_Tests.swift

---
+++ Tests iOS/Fab/fabPhoneNumberAsString_Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+import PhotoCaller
+
+class fabPhoneNumberAsString_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: String = fabPhoneNumberAsString()
+//        XCTAssert(!x.isEmpty)
+    }
+
+}
