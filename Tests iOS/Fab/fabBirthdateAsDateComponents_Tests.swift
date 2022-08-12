5ac257009784502d44c66e94f385c2f973809fd7
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 17:21:53 -0700

    Add fabBirthdateAsDateComponents_Tests.swift

---
+++ Tests iOS/Fab/fabBirthdateAsDateComponents_Tests.swift
@@ -0,0 +1,13 @@
+import XCTest
+import PhotoCaller
+
+class fabBirthdayDateComponents_Tests: XCTestCase {
+
+    func test() throws {
+//        let x: DateComponents = fabBirthdateDateComponents()
+//        XCTAssert(!x.year.isEmpty)
+//        XCTAssert(!x.month.isEmpty)
+//        XCTAssert(!x.day.isEmpty)
+    }
+
+}
