5eded48b9821e056d7e764916541a6d59e82cd01
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-11 19:49:26 -0700

    Add parseContactIdentifierStringToURLSafeString_Tests.swift

---
+++ Tests iOS/Parse/parseContactIdentifierStringToURLSafeString_Tests.swift
@@ -0,0 +1,12 @@
+import XCTest
+
+class parseContactIdentifierStringToURLSafeString_Tests: XCTestCase {
+
+//    func test_parseContactIdentifierStringToURLSafeString_OK() throws {
+//        let string = "1234567890-ABCDEF"
+//        let expect = "1234567890-ABCDEF"
+//        let actual = parseContactIdentifierStringToURLSafeString(string: string)
+//        XCTAssertEqual(expect, actual)
+//    }
+
+}
