4c44635232964c7d18ff367221ee96788de41ce9
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-15 08:28:53 -0700

    Add UIImage_emptyTests.swift

---
+++ Tests iOS/Extensions/UIImage_emptyTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+
+class UIImage_emptyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
