bb6d2fae187d296986929398d77981dd5508d850
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-15 09:12:17 -0700

    Add openViaMailToTests.swift

---
+++ Tests iOS/Open/openViaMailToTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+
+class openViaMailToTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
