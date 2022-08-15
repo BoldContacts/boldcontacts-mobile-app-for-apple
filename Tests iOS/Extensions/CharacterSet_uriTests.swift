81a11aa2a8880dca2c2a333cd942c152f0abb7b4
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-15 08:28:04 -0700

    Add CharacterSet_uriTests.swift

---
+++ Tests iOS/Extensions/CharacterSet_uriTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+
+class CharacterSet_uriTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
