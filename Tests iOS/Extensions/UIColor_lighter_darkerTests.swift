0966642be6348c3f3cb82563f5fa93ced4490863
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-15 08:28:31 -0700

    Add UIColor_lighter_darkerTests.swift

---
+++ Tests iOS/Extensions/UIColor_lighter_darkerTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+
+class UIColor_lighter_darkerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
