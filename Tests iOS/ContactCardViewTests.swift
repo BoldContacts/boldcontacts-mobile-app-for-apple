c75b640807d200c2beb5305c2e46b46073d24a42
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-15 08:33:18 -0700

    Add ContactCardViewTests.swift

---
+++ Tests iOS/ContactCardViewTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+
+class ContactCardViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
