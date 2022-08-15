f54031dd15e99195448d8038597b73a2b451fff9
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-15 08:34:38 -0700

    Add PersistenceTests.swift

---
+++ Tests iOS/PersistenceTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+
+class PersistenceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        self.measure {
+        }
+    }
+
+}
