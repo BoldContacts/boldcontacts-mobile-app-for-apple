05b9304859b635742dea298f3f1df466257c6c5e
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-19 10:25:46 -0700

    Add CNContact_intoThumbnail.swift

---
+++ Shared/Extensions/CNContact_intoThumbnail.swift
@@ -0,0 +1,24 @@
+import Contacts
+import UIKit
+
+extension CNContact {
+
+    /// Get this contact's UIImage.
+    ///
+    /// This implementation uses the contact's thumnail image data.
+    ///
+    /// Example:
+    ///
+    ///     let contact: CNContact = …
+    ///     let image: UIImage? = contact.intoUIImage()
+    ///
+    public func intoThumbnail() -> UIImage? {
+        if self.imageDataAvailable {
+            if let thumbnailImageData = self.thumbnailImageData {
+                return UIImage(data: thumbnailImageData)
+            }
+        }
+        return nil
+    }
+
+}
