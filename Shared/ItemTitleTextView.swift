6cb42a0b6cd76ae59176b69702fb609e51945af6
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-19 11:04:52 -0700

    Fix encapsulation in ItemTitleTextView.swift

--- Shared/ItemTitleTextView.swift
+++ Shared/ItemTitleTextView.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 import Contacts
 
-struct ItemTitleTextView: View {
-    @Binding var item: IntoTitle?
+struct ItemTitleTextView<T: IntoTitle>: View {
+    @Binding var item: T?
 
     var body: some View {
         intoText(item: item)
@@ -11,11 +11,13 @@ struct ItemTitleTextView: View {
             .font(Font.largeTitle.weight(.bold))
             .padding(.all, 0)
     }
+
+    func intoText(item: T?) -> Text? {
+        if let item = item {
+            return Text(item.intoTitle() ?? "?")
+        }
+        return Text("?")
+    }
+
 }
 
-func intoText(item: IntoTitle?) -> Text? {
-    if let item = item {
-        return Text(item.intoTitle() ?? "?")
-    }
-    return Text("?")
-}
