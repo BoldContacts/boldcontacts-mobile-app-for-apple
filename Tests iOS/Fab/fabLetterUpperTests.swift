af349183fe9a9c9abbe0a74c2cfcbf1283692e89
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-08-31 19:39:39 -0700

    Add fabLetterUpper.swift

---
+++ Tests iOS/Fab/fabLetterUpperTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BoldContacts
+
+class fabLetterUpperTests: XCTestCase {
+
+    func test() throws {
+//        let x: String = fabLetterUpper()
+//        XCTAssertEqual(1, x.count)
+    }
+
+}
