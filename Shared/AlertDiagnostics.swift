820a080e010c263535ce204e32602e6b26d6f498
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-10-28 13:20:53 -0700

    Add diagnostics alert

---
+++ Shared/AlertDiagnostics.swift
@@ -0,0 +1,8 @@
+//
+//  AlertDiagnostics.swift
+//  BoldContacts
+//
+//  Created by jph on 10/28/22.
+//
+
+import Foundation
---
+++ Shared/UserDefaults.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaults.swift
+//  BoldContacts
+//
+//  Created by jph on 10/27/22.
+//
+
+import Foundation
