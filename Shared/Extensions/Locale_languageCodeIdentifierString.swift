37b786a19af2d71d6bc20952e05f07ae85ff5caf
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-11-14 23:17:47 -0800

    Add Locale_languageCodeIdentifierString.swift

--- Shared/Extensions/Locale_languageCodeIdentifierString.swift
+++ Shared/Extensions/Locale_languageCodeIdentifierString.swift
@@ -4,13 +4,28 @@ extension Locale {
     
     public func languageCodeIdentifierString() -> String? {
         if #available(iOS 16, *) {
-            guard let languageCode  = self.language.languageCode else { return nil }
-            return languageCode.identifier
+            return languageCodeIdentifierStringWithIOS16()
         }
         if #available(iOS 15, *) {
-            guard let languageCode = self.languageCode else { return nil}
-            return languageCode
+            return languageCodeIdentifierStringWithIOS15()
         }
+        return nil
     }
-    
+
+    @available(iOS, introduced: 16)
+    func languageCodeIdentifierStringWithIOS16() -> String? {
+        guard let languageCode  = self.language.languageCode else { return nil }
+        let result = languageCode.identifier
+        logger.debug("\(#file) languageCodeIdentifierStringWithIOS16 -> \(result)")
+        return result
+    }
+
+    @available(iOS, introduced: 15, obsoleted: 16)
+    func languageCodeIdentifierStringWithIOS15() -> String? {
+        guard let languageCode = self.languageCode else { return nil}
+        let result = languageCode
+        logger.debug("\(#file) languageCodeIdentifierStringWithIOS15 -> \(result)")
+        return result
+    }
+
 }
