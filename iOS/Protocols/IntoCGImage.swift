991acb98c4a466813c41e427d3aec0fe1e6edaba
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-11-20 21:10:02 -0800

    Add IntoCGImage.swift

---
+++ iOS/Protocols/IntoCGImage.swift
@@ -0,0 +1,6 @@
+import Foundation
+import UIKit
+
+protocol IntoCGImage {
+    func intoCGImage() -> CGImage?
+}
---
+++ macOS/Protocols/IntoCGImage.swift
@@ -0,0 +1,6 @@
+import Foundation
+import AppKit
+
+protocol IntoCGImage {
+    func intoCGImage() -> CGImage?
+}
